67be9f3a935eb5d9cc3a62b4b83becca95fc2e78
Devi Pradeep Kumar Naredla <dnaredla@dnaredla-ltm.internal.salesforce.com>
2020-06-19 14:40:57 +0530

    Database_18th_6thattempt

--- server/database/index.js
+++ server/database/index.js
@@ -39,6 +39,7 @@ class Database {
         if(process.env.NODE_ENV === 'production')  
         {
             this._pool = new Pool({
+                user: 'bupotqlyaufdfd',
                 host: 'ec2-52-20-248-222.compute-1.amazonaws.com',
                 database: 'd53pse8l0okfjb',
                 password: 'a3888acada17d8d6fd7480759d46acd7143b001f1ad26c6f3950e53117f3d633',
