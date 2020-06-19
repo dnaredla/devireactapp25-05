b3be7b11e2b852784925d85175420cb4e8321a2c
Devi Pradeep Kumar Naredla <dnaredla@dnaredla-ltm.internal.salesforce.com>
2020-06-19 14:00:25 +0530

    Databasechanges19june

--- server/database/index.js
+++ server/database/index.js
@@ -2,26 +2,53 @@ var { Pool } = require('pg');
 
 const ENV = process.env.NODE_ENV;
 
-/*var CONNECTION_STRING;
 
-if(ENV === 'production'){
-    CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://username:pgpassword@db:5432/mydatabase';
-}
-else{
-    CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres:Asdf@1234@localhost:5432/dnaredla';
-}
-*/
 //const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres:Asdf@1234@localhost:5432/dnaredla';
-const CONNECTION_STRING = process.env.DATABASE_URL || 'postgres://bupotqlyaufdfd:a3888acada17d8d6fd7480759d46acd7143b001f1ad26c6f3950e53117f3d633@ec2-52-20-248-222.compute-1.amazonaws.com:5432/d53pse8l0okfjb'
-console.log('@@@@@'+CONNECTION_STRING);
+
+
+//console.log('@@@@@'+CONNECTION_STRING);
 const SSL = process.env.NODE_ENV === 'production';
 
 class Database {
      constructor () {
-this._pool = new Pool({
-connectionstring: CONNECTION_STRING,
-ssl: SSL
-})
+       
+       /* this._pool = new Pool({
+            user: 'bupotqlyaufdfd',
+            host: 'ec2-52-20-248-222.compute-1.amazonaws.com',
+            database: 'd53pse8l0okfjb',
+            password: 'a3888acada17d8d6fd7480759d46acd7143b001f1ad26c6f3950e53117f3d633',
+            port: 5432,
+            ssl: {rejectUnauthorized: false}
+          })*/
+        if(process.env.NODE_ENV === 'production')  
+        {
+            this._pool = new Pool({
+                user: 'bupotqlyaufdfd',
+                host: 'ec2-52-20-248-222.compute-1.amazonaws.com',
+                database: 'd53pse8l0okfjb',
+                password: 'a3888acada17d8d6fd7480759d46acd7143b001f1ad26c6f3950e53117f3d633',
+                port: 5432,
+                ssl: true
+              })
+        }
+        else{
+            this._pool = new Pool({
+                user: 'postgres',
+                host: 'localhost',
+                database: 'dnaredla',
+                password: 'Asdf@1234',
+                port: 5432
+              })
+        }
+         
+
+
+
+
+/*this._pool = new Pool({
+connectionstring: 'postgres://bupotqlyaufdfd:a3888acada17d8d6fd7480759d46acd7143b001f1ad26c6f3950e53117f3d633@ec2-52-20-248-222.compute-1.amazonaws.com:5432/d53pse8l0okfjb',
+ssl: false
+})*/
 
 this._pool.on('error',(err,client)=>{
     console.error('unexpected error on idle PostgreSQL client.',err);
@@ -32,7 +59,10 @@ this._pool.on('error',(err,client)=>{
 
      query (query, ...args) {
     this._pool.connect((err, client, done)=>{
-        if (err) throw err;
+        if (err){
+            console.log('@@@@@'+err.message);
+            throw err;
+        } 
         const params = args.length === 2 ? args[0] : [];
         const callback = args.length === 1 ? args[0] : args[1];
 
